848b31547c17c0cecf5e7786b4b0967d495a4335
eunjun <nb13232@navercorp.com>
2023-07-11 15:52:18 +0900

    기본

---
+++ src/test/a.spec.js
@@ -0,0 +1,16 @@
+describe("group comment", () => {
+
+    test("a equal 1", () => {
+        var a = 0;
+
+        expect(a).toBe(1);
+
+
+    });
+
+    test("test fail", () => {
+        throw new Error();
+    });
+});
+
+
